660a66fd83349b29faf8cc6af4d1a30d351a25b7
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-05-13 23:42:58 -0600

    Agarra recursos, solo los cuenta
    Se destruye el objeto

---
+++ Assets/Scripts/crearRecursos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crearRecursos : MonoBehaviour {
+
+	public GameObject Recursos;
+	public float tiempoCreacion =2f, RangoCreacion = 2f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Crear", 0.0f,tiempoCreacion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Crear(){
+		Vector3 SpawnPosition = new Vector3 (0,0,0);
+		SpawnPosition = this.transform.position + Random.onUnitSphere * RangoCreacion;
+		SpawnPosition = new Vector3 (SpawnPosition.x, SpawnPosition.y, SpawnPosition.z);
+
+		GameObject Recurso = Instantiate (Recursos, SpawnPosition, Quaternion.identity);
+
+	}
+}
---
+++ Assets/Scripts/sistemaRecursos.js
@@ -0,0 +1,30 @@
+﻿
+
+
+
+
+function OnGUI()//Lo que aparece en pantalla 
+{
+    GUILayout.Label( "  Recursos = " + Partida.puntos );
+
+}
+
+function OnTriggerEnter( other : Collider ) {
+    if (other.tag == "Recurso")//Si toca el tag "Recurso"  que suba puntos
+    {
+        Partida.puntos += 1; 
+        Destroy(other.gameObject);
+        
+    }
+}
+
+
+
+
+
+    public static class Partida
+    {
+        public var puntos=0;  //Puntos por los que comienza
+    
+    }
+	
