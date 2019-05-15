c3a0aa9b8e53b870fc80cc2585bbdfc3c424dff2
Andrea Arguello <arg17801@uvg.edu.gt>
2019-05-15 01:28:15 -0600

    Enemigo que sigue al personaje

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour {
+    private NavMeshAgent enemigo = null;
+    public Camera jugador = null;
+
+    public float attackDistance = 10.0f;
+    public float chaseDistance = 60.0f;
+    public float movespeed = 1.0f;
+    public float turnspeed = 45.0f;
+
+    public float damage = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        enemigo = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(enemigo.transform.position, jugador.transform.position);
+        if ( distance < chaseDistance)
+        {
+            Debug.Log(distance);
+            enemigo.SetDestination(jugador.transform.position);
+            if(distance < attackDistance)
+            {
+                //Bajar health del personaje
+            }
+        }
+	}
+}
