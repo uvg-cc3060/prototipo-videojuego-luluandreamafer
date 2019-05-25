9060b84b55deeda6b9a040d6d470b8cd7722a799
Andrea Arguello <arg17801@uvg.edu.gt>
2019-05-25 14:08:49 -0600

    Recoge armas (lanza)

---
+++ Assets/Scripts/RecogerArma.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogerArma : MonoBehaviour
+{
+    public Transform onHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (crearRecursos.hasWeapon==true)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+            this.transform.position = onHand.transform.position;
+            this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+        }
+    }
+}
--- Assets/Scripts/crearRecursos.cs
+++ Assets/Scripts/crearRecursos.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class crearRecursos : MonoBehaviour {
 
-	public GameObject Recursos;
+    public Transform onHand;
+    public GameObject Recursos;
+    public GameObject Flecha;
     public GameObject Jugador;
 	public float RangoCreacion = 20f;
     public int arbol = 0;
     private GUIStyle guiStyle = new GUIStyle();
     private int puntos = 0;
+    private int ammo = 0;
+    public static bool hasWeapon = false;
 
     // Use this for initialization
     void Start () {
 		
 	}
 
+
     private void OnGUI()
     {
-        GUI.Label(new Rect(100,0,100,50),"Recursos = " + puntos);
+        GUI.Label(new Rect(50,0,100,50),"Recursos = " + puntos);
+        GUI.Label(new Rect(Screen.width/2, 0, 100, 50), "Flechas = " + ammo);
         GUI.Label(new Rect(Screen.width - 100, 0, 100, 50), "Arboles =" + arbol);
         if (arbol >= 60) //aca deberia calcular la cantidad de arboles
         {
@@ -29,33 +35,55 @@ public class crearRecursos : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z)) //Z para plantar
         {
             if (puntos > 0)
             {
                 Invoke("Crear", 1.0f);
                 arbol += 1;
+                puntos -= 1;
             }
         }
-		
-	}
+        if (Input.GetKeyDown(KeyCode.X)) //X para disparar
+        {
+            if (hasWeapon)
+            {
+                Invoke("Atacar",1.0f);
+            }
+        }
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Entre");
         if (other.tag == "Recurso")//Si toca el tag "Recurso"  que suba puntos
         {
             puntos += 1;
-            Destroy(other.gameObject);
+            
 
+        }
+        if (other.tag == "Ammo")//Si toca el tag "Ammo" sube ammo
+        {
+            ammo += 1;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Weapon")//Indica que tiene arma
+        {
+            hasWeapon = true;
         }
     }
 
     public void Crear(){
-		Vector3 SpawnPosition = new Vector3 (0,0,0);
-		SpawnPosition = this.transform.position;
-		SpawnPosition = new Vector3 (transform.position.x - 10.0f, transform.position.y - 2.0f, SpawnPosition.z);
-
+        Vector3 SpawnPosition = onHand.transform.position;
+        this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
 		GameObject Recurso = Instantiate (Recursos, SpawnPosition, Quaternion.identity);
 
 	}
+
+    public void Atacar()
+    {
+        
+
+    }
 }
