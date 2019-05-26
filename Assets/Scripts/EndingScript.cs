5fa6d9606c46d4d7294d497cd5a83b8deeae9348
Andrea Arguello <arg17801@uvg.edu.gt>
2019-05-26 13:50:04 -0600

    Escena del final (está medio chafa :( )

--- Assets/Scripts/EndingScript.cs
+++ Assets/Scripts/EndingScript.cs
@@ -8,26 +8,34 @@ public class EndingScript : MonoBehaviour
 
     public static int total = crearRecursos.totalarboles + crearRecursos.arbol;
     int casualties = 0;
+    private bool winCondition = false;
+    private GUIStyle guiStyle = new GUIStyle();
 
     // Start is called before the first frame update
     void Start()
     {
         casualties = Random.Range(1, total);
+        if (crearRecursos.getFraccion())
+        {
+            winCondition = true;
+        }
     }
 
     private void OnGUI()
     {
-        
-        
-        if (crearRecursos.getFraccion())
+        string texto = "Una oleada de termitas arrasó con "+casualties+" árboles.\n";
+        if (winCondition)
         {
-
+            texto += "A pesar de esto, lograste sembrar al menos un tercio de árboles,\nasí que sobrevives (al menos otro día).";
         }
         else
         {
-
+            texto += "Sin embargo, no lograste sembrar al menos un tercio de árboles, así que,\ncon recursos insuficientes para poder seguir, no sobrevivirás.";
         }
-        
+        guiStyle.fontSize = 20;
+        guiStyle.alignment = TextAnchor.UpperCenter;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(-80, Screen.height / 2 - 25, 1000, 500), texto, guiStyle);
     }
 
     // Update is called once per frame
